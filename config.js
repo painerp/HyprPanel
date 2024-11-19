ed41b8d1cc0cbb10fda6246406080c7e971e3b7c
painerp <8081128+painerp@users.noreply.github.com>
2024-11-19 19:25:20 +0100

    fix ags version check

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ try {
             throw `"${bundler}" is not a valid bundler`;
     }
 
-    if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
+    if (v.ags[1] < v.expect[1] || (v.ags[1] === v.expect[1] && v.ags[2] < v.expect[2])) {
         print(`HyprPanel needs atleast v${v.expect.join('.')} of AGS, yours is v${v.ags.join('.')}`);
         App.quit();
     }
