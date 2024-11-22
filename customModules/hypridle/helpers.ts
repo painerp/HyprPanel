438a8bc9da6a7f99f662061328fef0e7951205ca
painerp <8081128+painerp@users.noreply.github.com>
2024-11-22 02:17:42 +0100

    switch hypridle commands to work with service

--- customModules/hypridle/helpers.ts
+++ customModules/hypridle/helpers.ts
@@ -1,29 +1,21 @@
-import { Variable as TVariable } from 'types/variable';
-
-export const isActiveCommand = `bash -c "pgrep -x "hypridle" > /dev/null && echo "yes" || echo "no""`;
+export const isActiveCommand = `bash -c "systemctl --user is-active hypridle.service"`;
 
 export const isActive = Variable(false);
 
-export const toggleIdle = (isActive: TVariable<boolean>): void => {
-    Utils.execAsync(isActiveCommand).then((res) => {
-        if (res === 'no') {
-            Utils.execAsync(`bash -c "nohup hypridle > /dev/null 2>&1 &"`).then(() => {
-                Utils.execAsync(isActiveCommand).then((res) => {
-                    isActive.value = res === 'yes';
-                });
-            });
-        } else {
-            Utils.execAsync(`bash -c "pkill hypridle "`).then(() => {
-                Utils.execAsync(isActiveCommand).then((res) => {
-                    isActive.value = res === 'yes';
-                });
-            });
-        }
-    });
+export const toggleIdle = (): void => {
+    Utils.execAsync(isActiveCommand)
+        .then(() => Utils.execAsync(`bash -c "systemctl --user stop hypridle.service"`).then(() => checkIdleStatus()))
+        .catch(() =>
+            Utils.execAsync(`bash -c "systemctl --user start hypridle.service"`).then(() => checkIdleStatus()),
+        );
 };
 
 export const checkIdleStatus = (): undefined => {
-    Utils.execAsync(isActiveCommand).then((res) => {
-        isActive.value = res === 'yes';
-    });
+    Utils.execAsync(isActiveCommand)
+        .then(() => {
+            isActive.value = true;
+        })
+        .catch(() => {
+            isActive.value = false;
+        });
 };
