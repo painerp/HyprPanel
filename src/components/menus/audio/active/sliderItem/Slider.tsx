f0e77df19f2f3440903a9804b502772bcced0007
painerp <8081128+painerp@users.noreply.github.com>
2024-12-26 23:34:54 +0100

    revert adding maximum volume to slider

--- src/components/menus/audio/active/sliderItem/Slider.tsx
+++ src/components/menus/audio/active/sliderItem/Slider.tsx
@@ -2,9 +2,6 @@ import { bind } from 'astal';
 import { Gdk, Gtk } from 'astal/gtk3';
 import AstalWp from 'gi://AstalWp?version=0.1';
 import { capitalizeFirstLetter, isScrollDown, isScrollUp } from 'src/lib/utils';
-import options from 'src/options';
-
-const { raiseMaximumVolume } = options.menus.volume;
 
 export const Slider = ({ device, type }: SliderProps): JSX.Element => {
     return (
@@ -25,7 +22,7 @@ export const Slider = ({ device, type }: SliderProps): JSX.Element => {
                 drawValue={false}
                 hexpand
                 min={0}
-                max={type === 'playback' ? bind(raiseMaximumVolume).as((raise) => (raise ? 1.5 : 1)) : 1}
+                max={1}
                 onDragged={({ value, dragging }) => {
                     if (dragging) {
                         device.volume = value;
@@ -36,8 +33,7 @@ export const Slider = ({ device, type }: SliderProps): JSX.Element => {
                     self.connect('scroll-event', (_, event: Gdk.Event) => {
                         if (isScrollUp(event)) {
                             const newVolume = device.volume + 0.05;
-                            const minVolume = raiseMaximumVolume.get() ? 1.5 : 1;
-                            device.set_volume(Math.min(newVolume, minVolume));
+                            device.set_volume(Math.min(newVolume, 1));
                         }
 
                         if (isScrollDown(event)) {
