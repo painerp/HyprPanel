bd286c85a56ffc477ece2c6ab739217598ce9015
painerp <8081128+painerp@users.noreply.github.com>
2024-12-26 23:33:39 +0100

    add hypridle nix support

--- src/components/bar/modules/hypridle/helpers/index.ts
+++ src/components/bar/modules/hypridle/helpers/index.ts
@@ -6,7 +6,7 @@ import { execAsync, Variable } from 'astal';
  * This command checks if the hypridle process is currently running by using the `pgrep` command.
  * It returns 'yes' if the process is found and 'no' otherwise.
  */
-export const isActiveCommand = `bash -c "pgrep -x 'hypridle' &>/dev/null && echo 'yes' || echo 'no'"`;
+export const isActiveCommand = `bash -c "systemctl --user is-active hypridle.service"`;
 
 /**
  * A variable to track the active state of the hypridle process.
@@ -14,33 +14,15 @@ export const isActiveCommand = `bash -c "pgrep -x 'hypridle' &>/dev/null && echo
 export const isActive = Variable(false);
 
 /**
- * Updates the active state of the hypridle process.
- *
- * This function checks if the hypridle process is currently running and updates the `isActive` variable accordingly.
- *
- * @param isActive A Variable<boolean> that tracks the active state of the hypridle process.
- */
-const updateIsActive = (isActive: Variable<boolean>): void => {
-    execAsync(isActiveCommand).then((res) => {
-        isActive.set(res === 'yes');
-    });
-};
-
-/**
  * Toggles the hypridle process on or off based on its current state.
  *
  * This function checks if the hypridle process is currently running. If it is not running, it starts the process.
  * If it is running, it stops the process. The active state is updated accordingly.
- *
- * @param isActive A Variable<boolean> that tracks the active state of the hypridle process.
  */
-export const toggleIdle = (isActive: Variable<boolean>): void => {
-    execAsync(isActiveCommand).then((res) => {
-        const toggleIdleCommand =
-            res === 'no' ? `bash -c "nohup hypridle > /dev/null 2>&1 &"` : `bash -c "pkill hypridle"`;
-
-        execAsync(toggleIdleCommand).then(() => updateIsActive(isActive));
-    });
+export const toggleIdle = (): void => {
+    execAsync(isActiveCommand)
+        .then(() => execAsync(`bash -c "systemctl --user stop hypridle.service"`).then(() => checkIdleStatus()))
+        .catch(() => execAsync(`bash -c "systemctl --user start hypridle.service"`).then(() => checkIdleStatus()));
 };
 
 /**
@@ -48,8 +30,12 @@ export const toggleIdle = (isActive: Variable<boolean>): void => {
  *
  * This function checks if the hypridle process is currently running and updates the `isActive` variable accordingly.
  */
-export const checkIdleStatus = (): undefined => {
-    execAsync(isActiveCommand).then((res) => {
-        isActive.set(res === 'yes');
-    });
+export const checkIdleStatus = (): void => {
+    execAsync(isActiveCommand)
+        .then(() => {
+            isActive.set(true);
+        })
+        .catch(() => {
+            isActive.set(false);
+        });
 };
